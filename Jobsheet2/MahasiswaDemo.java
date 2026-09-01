ebc3485488acde957d61d082b4d242e39d4e515d
Agam Darmawan <madridlover085@gmail.com>
2026-09-01 11:43:31 +0700

    update

--- Jobsheet2/MahasiswaDemo.java
+++ Jobsheet2/MahasiswaDemo.java
@@ -3,20 +3,20 @@ public class MahasiswaDemo {
     public static void main(String[] args) {
         // Object 1
         Mahasiswa m1 = new Mahasiswa();
-        m1.nim = "023432";
-        m1.nama = "Yansy Ayuningtyas";
-        m1.alamat = "Nias, Sumatera Utara";
-        m1.kelas = "2A";
+        m1.nim = "254107060108";
+        m1.nama = "Agam Yoga Darmawan";
+        m1.alamat = "Malang, Jawa Timur";
+        m1.kelas = "SIB-2G";
         m1.displayBiodata();
 
         System.out.println("---------------------------");
 
         // Object 2 (Step 12)
         Mahasiswa m2 = new Mahasiswa();
-        m2.nim = "023433";
-        m2.nama = "Budi Santoso";
-        m2.alamat = "Malang, Jawa Timur";
-        m2.kelas = "2B";
+        m2.nim = "254107060109";
+        m2.nama = "Muhammad Iam Nugroho";
+        m2.alamat = "Batu, Jawa Timur";
+        m2.kelas = "SIB-2G";
         m2.displayBiodata();
     }
 }
